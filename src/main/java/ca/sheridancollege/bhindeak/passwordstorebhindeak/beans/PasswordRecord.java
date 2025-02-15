a1559d5939b1ad76d6775f3c62565eb69def2015
Aksheen Bhinder <bhindeak@sheridancollege.ca>
2025-02-14 20:37:19 -0500

    Password Record class created

--- src/main/java/ca/sheridancollege/bhindeak/passwordstorebhindeak/beans/PasswordRecord.java
+++ src/main/java/ca/sheridancollege/bhindeak/passwordstorebhindeak/beans/PasswordRecord.java
@@ -1,4 +1,22 @@
 package ca.sheridancollege.bhindeak.passwordstorebhindeak.beans;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "passwords")
 public class PasswordRecord {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @NonNull
+    private Long id;
+    private String title;
+    private String username;
+    private String password;
+    private String url;
+    private String email;
+    private String notes;
 }
